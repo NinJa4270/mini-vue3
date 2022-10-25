781174c819491ba903a88b2452f280447941407e
NinJa427 <17398081021@163.com>
2022-10-25 22:54:38 +0800

    init

---
+++ src/reactivity/tests/index.spec.ts
@@ -0,0 +1,9 @@
+import { expect, test, describe } from 'vitest'
+
+
+describe('reactivity', () => {
+    test('init', () => {
+        expect(true).toBe(true)
+        expect(1+1).toBe(2)
+    })
+})
