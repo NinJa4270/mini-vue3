515c8be2604ad9803f04c31c7169a50f747db8c5
NinJa427 <17398081021@163.com>
2022-10-26 17:26:49 +0800

    isProxy

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -38,3 +38,8 @@ export function isReactive(value: unknown): boolean {
 export function isReadonly(value: unknown): boolean {
     return !!(value as Target)[ReactiveFlags.IS_READONLY]
 }
+
+
+export function isProxy(value: unknown): boolean {
+    return isReactive(value) || isReadonly(value)
+}
---
+++ src/reactivity/tests/isProxy.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test, describe } from 'vitest'
+import { isProxy, reactive, readonly, shallowReadonly } from '../reactive'
+
+describe('isProxy', () => {
+
+    test('isProxy', () => {
+        const original = { foo: 1, bar: { baz: 2 } }
+        const observed = reactive(original)
+        const warpped = readonly(original)
+        const shallowWarpped = shallowReadonly(original)
+
+        expect(isProxy(original)).toBe(false)
+        expect(isProxy(observed)).toBe(true)
+        expect(isProxy(warpped)).toBe(true)
+        expect(isProxy(shallowWarpped)).toBe(true)
+        expect(isProxy(shallowWarpped.bar)).toBe(false)
+    })
+})
